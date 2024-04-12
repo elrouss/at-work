bc886d632b4ea1441527411bed11641cab4f5ca9
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2024-04-12 11:08:17 +0300

    deploy - fix path with json fetch

--- src/helpers/data/getJsonData.ts
+++ src/helpers/data/getJsonData.ts
@@ -1,7 +1,6 @@
 export const getJsonData = async (name: string) => {
   try {
-    // особенности деплоя на GH
-    const file = await fetch(`../../../at-work/dist/assets/data/${name}.json`);
+    const file = await fetch(`assets/data/${name}.json`);
     if (!file) throw new Error();
 
     return await file.json();
